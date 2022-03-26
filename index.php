e013534315ca4c4406723f77ddae05436c4e7602
Vismay Patel <vismaypikachu@gmail.com>
2022-03-26 00:36:00 +0000

    test

--- index.php
+++ index.php
@@ -10,7 +10,34 @@
     <!-- <link rel="icon" type="image/x-icon" href="/src/images/ICC LOGO.svg"> -->
 </head>
 <body>
-   <?php include('navbar.php'); ?>
+<nav class="navbar navbar-dark bg-dark">
+        <div class="container-fluid">
+            <a class="navbar-brand" href="#">Vismay Patel</a>
+            <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarText" aria-controls="navbarText" aria-expanded="false" aria-label="Toggle navigation">
+                <span class="navbar-toggler-icon"></span>
+            </button>
+            <div class="collapse navbar-collapse" id="navbarText">
+                <ul class="navbar-nav me-auto mb-2 mb-lg-0">
+                    <li class="nav-item">
+                        <a class="nav-link active" aria-current="page" href="#">Home</a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link" href="#">Certifications</a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link" href="#">Projects</a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link" href="#">Contact Me</a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link" href="#">My 501(C)3 Nonprofit</a>
+                    </li>
+                </ul>
+                <span class="navbar-text"></span>
+            </div>
+        </div>
+    </nav>
 
 
 

68b0c3ac3309a89e7e78820c0836daf3b31298d8
Vismay Patel <vismaypikachu@gmail.com>
2022-03-28 18:49:36 +0000

    test

--- index.php
+++ index.php
@@ -16,7 +16,7 @@
             <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarText" aria-controls="navbarText" aria-expanded="false" aria-label="Toggle navigation">
                 <span class="navbar-toggler-icon"></span>
             </button>
-            <div class="collapse navbar-collapse" id="navbarText">
+            <!-- <div class="collapse navbar-collapse" id="navbarText"> -->
                 <ul class="navbar-nav me-auto mb-2 mb-lg-0">
                     <li class="nav-item">
                         <a class="nav-link active" aria-current="page" href="#">Home</a>
@@ -35,7 +35,7 @@
                     </li>
                 </ul>
                 <span class="navbar-text"></span>
-            </div>
+            <!-- </div> -->
         </div>
     </nav>
 

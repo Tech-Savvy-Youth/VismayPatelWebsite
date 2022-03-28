203ddd8c6a729320f98ddf1bf0dc00826474d55d
Vismay Patel <vismaypikachu@gmail.com>
2022-03-28 20:37:54 +0000

    test

--- index.php
+++ index.php
@@ -57,7 +57,8 @@
 
 
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-ka7Sk0Gln4gmtz2MlQnikT1wXgYsOg+OMhuP+IlRH9sENBO0LRn5q+8nbTov4+1p" crossorigin="anonymous"></script>
-    <script src="three.r119.min.js"></script>
+</body>
+<script src="three.r119.min.js"></script>
     <script src="vanta.halo.min.js"></script>
     <script>
         VANTA.HALO({
@@ -70,5 +71,4 @@
         baseColor: 0xde0572
         })
     </script>
-</body>
 </html>

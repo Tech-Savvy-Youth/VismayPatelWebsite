7f5c6fc22d7adb2624b61549aac9222239d03f29
Vismay Patel <vismaypikachu@gmail.com>
2022-03-28 20:57:56 +0000

    background

--- index.php
+++ index.php
@@ -61,8 +61,9 @@
 
 
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-ka7Sk0Gln4gmtz2MlQnikT1wXgYsOg+OMhuP+IlRH9sENBO0LRn5q+8nbTov4+1p" crossorigin="anonymous"></script>
-    <script src="three.r119.min.js"></script>
-    <script src="vanta.halo.min.js"></script>
+    
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/three.js/r121/three.min.js"></script>
+    <script src="https://cdn.jsdelivr.net/npm/vanta@latest/dist/vanta.halo.min.js"></script>
     <script>
         VANTA.HALO({
         el: "#background",
